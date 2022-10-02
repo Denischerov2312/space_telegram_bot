6de7468950c2979ea2140b3ad0e242252a011c54
Denischerov2312 <hshshsdbhshs@gmail.com>
2022-10-02 18:35:45 +0300

    Создал скрипт для скачивания изображений spacex

---
+++ fetch_spacex_images.py
@@ -0,0 +1,33 @@
+import argparse
+
+from fetch_images_api import download_picture
+
+import requests
+
+
+def get_flight_id():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--flight_id', help='ID запуска SpaceX', default='latest')
+    args = parser.parse_args()
+    return args.flight_id
+
+
+def get_spacex_links():
+    url = f'https://api.spacexdata.com/v5/launches/{get_flight_id()}'
+    response = requests.get(url)
+    response.raise_for_status()
+    return response.json()['links']['flickr']['original']
+
+
+def fetch_spacex_last_launch():
+    for photo_number, link in enumerate(get_spacex_links()):
+        filepath = f'images/spacex{photo_number}.jpg'
+        download_picture(link, filepath)
+
+
+def main():
+    fetch_spacex_last_launch()
+
+
+if __name__ == '__main__':
+    main()
