ee367cdce11827c1d46d676666dd446a84534308
Denischerov2312 <hshshsdbhshs@gmail.com>
2022-10-08 15:18:34 +0300

    Отправил фото в телеграмм канал

--- telegram_bot.py
+++ telegram_bot.py
@@ -4,12 +4,14 @@ import os
 import telegram
 from dotenv import load_dotenv
 
+CHAT_ID = '@SpacePhotos12'
+
 
 def main():
     load_dotenv()
     telegram_token = os.getenv('TELEGRAM_BOT_TOKEN')
     bot = telegram.Bot(token=telegram_token)
-    bot.send_message(chat_id='@SpacePhotos12', text='Hello, world!')
+    bot.send_photo(chat_id=CHAT_ID, photo=open('images/epic_nasa0.png', 'rb'))
 
 if __name__ == '__main__':
     main()
