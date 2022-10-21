d3e7979e293da848b4e4a1fcf5d97fd19b7fe91d
Denischerov2312 <hshshsdbhshs@gmail.com>
2022-10-21 15:35:33 +0300

    Написал скрипт, публикующий фотографию,
    Публикует в чат любую выбранную фотографию, если путь до фото не указан, публикуется одна случайная фотография из папки images

---
+++ bot_send_image.py
@@ -0,0 +1,34 @@
+import os
+import argparse
+import random
+
+import telegram
+
+from fetch_images_api import get_images_paths
+from dotenv import load_dotenv
+load_dotenv()
+
+
+def get_image_path():
+    parser = argparse.ArgumentParser()
+    default_path = random.choice(get_images_paths())
+    parser.add_argument(
+        '-I',
+        '--image',
+        default=default_path,
+        help='Путь до изображения'
+        )
+    args = parser.parse_args()
+    return args.image
+
+
+def main():
+    telegram_token = os.getenv("TELEGRAM_BOT_TOKEN")
+    chat_id = os.getenv("TELEGRAMM_CHAT_ID")
+    bot = telegram.Bot(token=telegram_token)
+    image = get_image_path()
+    bot.send_photo(chat_id=chat_id, photo=open(image, 'rb'))
+
+
+if __name__ == '__main__':
+    main()
